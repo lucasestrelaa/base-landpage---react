5b75c85251fb67079a5c55ddfb7a167fbe97f74c
STATION955\Analista19 <analista19.ti@connectsa.com.br>
2024-01-23 13:48:09 -0300

    atualizacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Section from './components/section';
 
 function App() {
   return (
     <div className="App">
       <p>teste</p>
+      <Section />
     </div>
   );
 }
